25d559ed980342b3489037978e4ba3ac20093a00
DanChab <109275451+dopbip@users.noreply.github.com>
2023-05-04 15:45:36 +0200

    Update .env.js

--- .env.js
+++ .env.js
@@ -8,7 +8,7 @@ const development = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     PORT: '8000',
     Meta_WA_VerifyToken: 'panda_in_space',
-    Meta_WA_accessToken: 'EAAICMPLTiqUBAMo6dFeIpAAK7uZBT1jZABVZBh00iNchJaNnELGoPycnxVdyolearZAlgwX6uxeUGIyb3lUWQHdcuxzlkc7N6mCi8XNw27oZARUxaFf7s1Q8MlqoG3jVZBddZBHuJNPFZBgrIVeAN4xQE0ZCIcE2drqiUo0r0J6fsPZA2Y1sjRzqJgeGsacorYWFyc5A5w3N31v1jxH2KvGcz8',
+    Meta_WA_accessToken: 'EAAICMPLTiqUBACkDS295GYCxYMgHdXheHaPnXiIa2WWL8Cl1ZAONUi4nkC7UGE1FlkGwttzrCxDPNmSOtB6kuZACuV05PjZCF2fXoGOxt7iw1P3TFv8oOGgkXezBx9xQy8XN9FZACZCQa8WoC9jJu3US2DBfFLZCeZBUx7Y2Tsi7gEUFtjZCqDBQ7a2CaDVVHOZAYswt0MSedjYbkAeBW8hsj',
     Meta_WA_SenderPhoneNumberId: '118618771205644',
     Meta_WA_wabaId: '118416884561172',
 };
