947182a418c66823aa3abc60aabf50c73fa3c309
dopbip <dopbipdev@gmail.com>
2023-05-11 09:42:00 +0200

    MongoDB implemented

--- app.js
+++ app.js
@@ -6,12 +6,22 @@ const bodyParser = require('body-parser');
 let indexRoutes = require('./routes/index.js');
 
 const main = async () => {
+    try {
+        mongoose.Promise = global.Promise;
+        mongoose.connect(process.env.ATLAS_URL, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useFindAndModify: false
+        });
+      } catch (err) {
+        console.log('Mongoose error', err);
+      }
     const app = express();
     app.use(bodyParser.json());
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
     app.use('/', indexRoutes);
-    app.use('*', (req, res) => res.status(404).send('404 Hot Found'));
+    app.use('*', (req, res) => res.status(404).send('404 Hot Found :) that a joke (Not found)'));
     app.listen(port, () =>
         console.log(`App now running and listening on port ${port}`)
     );
