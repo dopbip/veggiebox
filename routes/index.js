703e2730774735d1fd5c06975d556ede2c52eb79
dopbip <dopbipdev@gmail.com>
2023-05-11 17:55:07 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -68,7 +68,7 @@ router.post('/meta_wa_callbackurl', async (req, res) => {
             let typeOfMsg = incomingMessage.type; // extract the type of message (some are text, others are images, others are responses to buttons etc...)
             let message_id = incomingMessage.message_id; // extract the message id
             console.log("))))))incomingMessage)))))")
-            //console.log(incomingMessage)
+            console.log(messageContent)
              // Create a new session ID using the WhatsApp phone number
             const sessionId = recipientPhone.split('@')[0];
             // Create a new Dialogflow session client using the service account key
