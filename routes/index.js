87fb45d26026e8c0bfbdf6ea95e397ee55885ed0
dopbip <dopbipdev@gmail.com>
2023-05-14 14:44:37 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -131,7 +131,7 @@ router.post('/meta_wa_callbackurl', async (req, res) => {
             //Get button id
             if (typeOfMsg === 'simple_button_message') {
                 let button_id = incomingMessage.button_reply.id;
-                let message = `*FRUITS*\n`
+                let message = `*FRUITS*\n\n`
                 switch (button_id) {
                     case 'fruit_category':
                         let listOfProducts = await Store.getProductsInCategory(button_id);
@@ -140,7 +140,7 @@ router.post('/meta_wa_callbackurl', async (req, res) => {
                             let name = product.name
                             let packPrice = product.pack_price
                             let packedItems = product.packed_items
-                            message += `${emoji} ${name} Pack of ${packedItems}  k${packPrice}\n\n`
+                            message += `${emoji} *${name} Pack of ${packedItems}  k${packPrice}*\n\n`
                         })
                         message += `\nTell me which fruits you want and how many packs.\n_eg. 2 packs banana_`
                         const reply = {
