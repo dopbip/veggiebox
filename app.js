9e60c011e7e8522e9ab71d0c12e88a8088e732b1
dopbip <dopbipdev@gmail.com>
2023-05-14 13:34:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
 process.env = require('./.env.js')(process.env.NODE_ENV || 'development');
 const port = process.env.PORT || 9000;
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 let indexRoutes = require('./routes/index.js');
 
 const main = async () => {
-    try {
-        mongoose.Promise = global.Promise;
-        mongoose.connect(process.env.ATLAS_URL, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-          useFindAndModify: false
-        });
-      } catch (err) {
-        console.log('Mongoose error', err);
-      }
     const app = express();
     app.use(bodyParser.json());
     app.use(express.json());
